12875ec5c399208cf5eabc313b63e06140b9383d
Salim Miah <141350879+salim-miah@users.noreply.github.com>
2023-09-23 22:53:37 +0600

    Add files via upload
    This is a simple calculator app which is made using Tkinter. The calculator only works for two operands.

---
+++ Calculator app.py
@@ -0,0 +1,105 @@
+from tkinter import *
+
+
+class Numbers:
+    row = 2
+    col = 0
+
+    def __init__(self, number):
+        self.number = number
+        if number != 0:
+            button = Button(root, text=str(number), padx=35, pady=30, font=("Arial",15), bg="Grey", command=self.click)
+            if Numbers.col < 3:
+                button.grid(row=Numbers.row, column=Numbers.col)
+                Numbers.col += 1
+            else:
+                Numbers.col = 0
+                Numbers.row += 1
+                button.grid(row=Numbers.row, column=Numbers.col)
+                Numbers.col += 1
+        else:
+            button = Button(root, text=str(number), padx=132, pady=30, font=("Arial",15), bg="Grey", command=self.click)
+            button.grid(row=5, column=0, columnspan=3)
+
+    def click(self):
+        global entry_index
+        entry.insert(entry_index, str(self.number))
+        entry_index += 1
+
+
+class Calculator:
+    def __init__(self):
+        self.operator = None
+        self.first_operand = None
+        self.second_operand = None
+
+    def calculate(self):
+        if self.operator == "+":
+            return self.first_operand+self.second_operand
+        elif self.operator == "-":
+            return self.first_operand-self.second_operand
+        elif self.operator == "×":
+            return self.first_operand*self.second_operand
+        else:
+            return self.first_operand / self.second_operand
+
+
+class Operators:
+    row = 1
+
+    def __init__(self, operator):
+        self.operator = operator
+        button = Button(root, text=operator, padx=30, pady=30, font=("Arial",15), bg="Grey", command=self.click)
+        button.grid(row=Operators.row, column=3)
+        Operators.row += 1
+
+    def click(self):
+        if self.operator != "=":
+            calculator.first_operand = int(entry.get())
+            calculator.operator = self.operator
+            clear.click()
+        else:
+            calculator.second_operand = int(entry.get())
+            result = calculator.calculate()
+            clear.click()
+            entry.insert(0, result)
+
+
+class Clear:
+    def __init__(self):
+        button = Button(root, text="Clear", padx=115, pady=30, font=("Arial",15), command=self.click, bg="Orange")
+        button.grid(row=1, column=0, columnspan=3)
+
+    def click(self):
+        global entry_index
+        entry.delete(0, entry_index+1)
+        entry_index = 0
+
+
+root=Tk()
+root.title("Salim's Calculator")
+
+entry=Entry(root,width=20,borderwidth=5,font=('Arial',25))
+entry.grid(row=0,column=0,columnspan=4)
+entry_index=0
+calculator=Calculator()
+number1=Numbers(1)
+number2=Numbers(2)
+number3=Numbers(3)
+number4=Numbers(4)
+number5=Numbers(5)
+number6=Numbers(6)
+number7=Numbers(7)
+number8=Numbers(8)
+number9=Numbers(9)
+number0=Numbers(0)
+operator1=Operators("÷")
+operator2=Operators("×")
+operator3=Operators("-")
+operator4=Operators("+")
+operator5=Operators("=")
+clear=Clear()
+
+root.mainloop()
+
+
